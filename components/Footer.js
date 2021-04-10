7d3d0bd15335c1e3d643374f6baad1aa52ef6c7e
ozorku <adavizeozorku@gmail.com>
2021-04-10 09:35:49 +0100

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 const Footer = () => {
   return (
     <Flex
@@ -8,10 +8,12 @@ const Footer = () => {
       borderTop="1px solid rgb(234, 234, 234)"
       paddingY={5}
     >
-      Created by
-      <a target="_blank" href="https://github.com/ize-302">
-        <b> Ize-302</b>
-      </a>
+      <Text>
+        Source code on{" "}
+        <a target="_blank" href="https://github.com/ize-302/manage-repos">
+          <b>Github</b>
+        </a>
+      </Text>
     </Flex>
   );
 };
